3e2eccea53823c6f32d7d81abd7f73caaa9f9a13
mattdeboard <matt.deboard@gmail.com>
2011-02-01 15:34:56 -0500

    lowercasing for the domain portion of the URL added to the MyUrlField class.

--- yuk/models.py
+++ yuk/models.py
@@ -2,13 +2,32 @@ from django.db import models
 from django.forms import ModelForm
 from django import forms
 from tagging.forms import TagField
+from urlparse import urlparse, urlunparse
 import sys
 
 class MyUrlField(forms.URLField):
         # convert this-part.com to lowercase, but not xyz.com/ThisPart
         def to_python(self, value):
                 '''Lowercase the URL input for validation.'''
-                return value.lower()
+                if value.startswith('http://'):
+                        lval = self.lowercase_domain(value)
+                        return lval
+                else:
+                        lval = self.lowercase_domain('http://%s' % value)
+                        return lval
+
+        def lowercase_domain(self, url):
+                parsed = urlparse(url)
+                retval = urlunparse((parsed.scheme,
+                                     parsed.netloc.lower(),
+                                     parsed.path,
+                                     parsed.params,
+                                     parsed.query,
+                                     parsed.fragment))
+                if url.endswith('?') and not retval.endswith('?'):
+                        retval += '?'
+                return retval
+                
 
 class Url(models.Model):
 
