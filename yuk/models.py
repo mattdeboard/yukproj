15b257af141f4842efb69da64be535a235e2962f
mattdeboard <matt.deboard@gmail.com>
2011-02-01 15:08:26 -0500

    Changed MyUrlField so it subclasses forms.URLField instead of models.URLField. This made the to_python() overwrite evaluate the lowercase domain for uniqueness, instead of allowing nonsense like Twitter.com and twitter.com to both exist in the db.

--- yuk/models.py
+++ yuk/models.py
@@ -4,11 +4,9 @@ from django import forms
 from tagging.forms import TagField
 import sys
 
-class MyUrlField(models.URLField):
+class MyUrlField(forms.URLField):
         # convert this-part.com to lowercase, but not xyz.com/ThisPart
-        def post_init(self, model_instance):
-                super(MyUrlField, self).post_init(model_instance)
-                value = getattr(model_instance, self.attname)
+        def to_python(self, value):
                 '''Lowercase the URL input for validation.'''
                 return value.lower()
 
@@ -17,13 +15,13 @@ class Url(models.Model):
 	def __unicode__(self):
 		return self.url
 
-	url = MyUrlField(verify_exists=True, unique=True)
+	url = models.URLField(verify_exists=True, unique=True)
 	url_name = models.CharField(max_length=200)
 	tagstring = models.CharField(max_length=200)
 	url_desc = models.TextField()
         
 class UrlForm(ModelForm):
-        url = forms.URLField(label='URL:')
+        url = MyUrlField(label='URL:')
 	url_name = forms.CharField(label='Name:')
 	tagstring = forms.CharField(label='tags separated by commas:')
 	url_desc = forms.CharField(label='Description (max 500 chars):', widget=forms.Textarea)
