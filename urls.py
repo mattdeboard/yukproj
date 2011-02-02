82c99d2d0e4c257b85cb2fdb439ed233400618a2
mattdeboard <matt.deboard@gmail.com>
2011-02-02 12:31:57 -0500

    Unbuggered urls.py

--- urls.py
+++ urls.py
@@ -6,11 +6,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^yuk/$', 'yuk.views.index'),
-    (r'^yuk/new_url/', 'yuk.views.new_url'),
-    (r'^yuk/(?P<tag>\w+)/$', 'yuk.views.tag_detail'),
+    
+    (r'^new_url/', 'yuk.views.new_url'),
+    
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^$', 'yuk.views.index'),
+    (r'^(?P<tag>\w+)/$', 'yuk.views.tag_detail'),
 ##    url(r'^yuk/tag/(?P<tag>[^/]+)/$',
 ##        tagged_object_list,
 ##        dict(model=Url, paginate_by=10, allow_empty=True,
