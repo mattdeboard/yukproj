e2c4c0d8141f8d1f6a69bb8a2a13a02b7af7406d
Matt DeBoard <matt.deboard@gmail.com>
2011-05-06 17:37:49 -0400

    Added separate Python & unix error handling

--- srchupdate.py
+++ srchupdate.py
@@ -13,20 +13,23 @@ def update():
                         datefmt='%m/%d/%Y %I:%M:%S %p')
     logging.info('Starting index update.')
     try:
-        status = os.system("cd %s; . bin/activate; cd %s; sudo chown matt:matt %s; "
-                           "sudo chown matt:matt %s*; ./manage.py update_index; sud"
-                           "o chown www-data:www-data %s; sudo chown www-data:www-d"
-                           "ata %s*; sudo /etc/init.d/apache2 force-reload" % 
-                           (domain_dir, appdir, whoosh_dir, whoosh_dir, whoosh_dir, 
-                            whoosh_dir))
+        status = os.system("cd %s; . bin/activate; cd %s; sudo chown matt:matt "
+                           "%s; sudo chown matt:matt %s*; ./manage.py update_in"
+                           "dex; sudo chown www-data:www-data %s; sudo chown ww"
+                           "w-data:www-data %s*; sudo /etc/init.d/apache2 force"
+                           "-reload" % (domain_dir, appdir, whoosh_dir, 
+                                        whoosh_dir, whoosh_dir, whoosh_dir))
+
         if status == 0:
             logging.info('Index successfully updated.')
         else:
-            logging.error("Index update failed. Please consult UNIX exit status val"
-                          "ues for more information.")
+            logging.error("Index update failed. Please consult UNIX exit status"
+                          " values for more information.")
             logging.error("Exit status: %s" % status)
     except:
-        logging.error("Exception received:", sys.exc_info()[0])
+        logging.error("Exception received: ", 
+                      sys.exc_info()[0], 
+                      sys.exc_info()[1])
 
 
 if __name__ == '__main__':
