152ddd6bcf88d91bed3daf3dc1647eff2d0ce2ab
Matt DeBoard <matt.deboard@gmail.com>
2011-05-06 17:22:31 -0400

    A little smarter on the error handling.

--- srchupdate.py
+++ srchupdate.py
@@ -10,17 +10,16 @@ def update():
                         level=logging.INFO,
                         format='%(asctime)s %(message)s', 
                         datefmt='%m/%d/%Y %I:%M:%S %p')
-    logging.info('Starting index update.')
-    try:
-        os.system("cd %s; . bin/activate; cd %s; sudo chown matt:matt %s; sudo "
-                  "chown matt:matt %s*; ./manage.py update_index; sudo chown ww"
-                  "w-data:www-data %s; sudo chown www-data:www-data %s*; sudo /"
-                  "etc/init.d/apache2 force-reload" % (domain_dir, appdir, 
-                                                       whoosh_dir, whoosh_dir,
-                                                       whoosh_dir, whoosh_dir))
-        logging.info('Index successfully updated.')
-    except:
-        logging.info('Index update failed.')
+#    logging.info('Starting index update.')
+    status = os.system("cd %s; . bin/activate; cd %s; sudo chown matt:matt %s; "
+                       "sudo chown matt:matt %s*; ./manage.py update_index; sud"
+                       "o chown www-data:www-data %s; sudo chown www-data:www-d"
+                       "ata %s*; sudo /etc/init.d/apache2 force-reload" % 
+                       (domain_dir, appdir, whoosh_dir, whoosh_dir, whoosh_dir, 
+                        whoosh_dir))
+#    logging.info('Index successfully updated.')
+    logging.info("Exit status: %s" % status)
+
 
 if __name__ == '__main__':
     update()
