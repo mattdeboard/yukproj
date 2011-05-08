5c28288d739caef1c74e805e332e751d2d07b9c6
Matt DeBoard <matt.deboard@gmail.com>
2011-05-07 20:53:12 -0400

    Missed a couple spaces in my call() routines, broke my script.

--- srchupdate.py
+++ srchupdate.py
@@ -22,7 +22,8 @@ def update():
                                       'www-data:www-data', whoosh_dir])
         apachereload = subprocess.call(['sudo', 
                                         '/etc/init.d/apache2', 'force-reload'])
-        if sum([mattwhoosh, update_index, apachewhsh, apachereload]) == 0:
+        if sum([int(mattwhoosh), int(update_index), int(apachewhsh), 
+                int(apachereload)]) == 0:
             logging.info('Index successfully updated.')
             logging.error('The following exit codes were returned:')
             logging.error('- mattwhoosh: %s' % mattwhoosh)
