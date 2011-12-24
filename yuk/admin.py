9e5e087211d03df72b7c17538fb43b95cd2dadeb
Matt DeBoard <matt.deboard@gmail.com>
2011-12-24 18:17:00 -0500

    Change list view for model admin.

--- yuk/admin.py
+++ yuk/admin.py
@@ -1,4 +1,8 @@
 from yuk.models import Item
 from django.contrib import admin
 
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('date_created', 'user', 'displays', 'item_type')
+
 admin.site.register(Item)
